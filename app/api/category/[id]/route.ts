e4dadb5e6e41551f391788c90a142a2e154247a3
hedi <amirhedi054@email.com>
2025-04-20 15:02:17 +0100

    second commit

--- app/api/category/[id]/route.ts
+++ app/api/category/[id]/route.ts
@@ -1,22 +1,27 @@
 // app/api/category/[id]/route.ts
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import prisma from '../../../lib/prisma';
 
-export async function DELETE(req: Request, { params }: { params: { id: string } }) {
-  const categoryId = parseInt(params.id);
-
-  try {
-    await prisma.item.deleteMany({
-      where: { categoryId },
-    });
-
-    await prisma.category.delete({
-      where: { id: categoryId },
-    });
-
-    return NextResponse.json({ success: true });
-  } catch (error) {
-    console.error('Error deleting category:', error);
-    return new NextResponse('Failed to delete category', { status: 500 });
+export async function DELETE(
+    req: NextRequest,
+    { params }: { params: { id: string } }
+  ) {
+    const categoryId = parseInt(params.id);
+  
+    try {
+      // Delete all items associated with this category
+      await prisma.item.deleteMany({
+        where: { categoryId },
+      });
+  
+      // Then delete the category itself
+      await prisma.category.delete({
+        where: { id: categoryId },
+      });
+  
+      return NextResponse.json({ success: true });
+    } catch (error) {
+      console.error('Error deleting category:', error);
+      return new NextResponse('Failed to delete category', { status: 500 });
+    }
   }
-}
